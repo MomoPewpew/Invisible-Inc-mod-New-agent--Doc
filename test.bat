c6f24b7597773008f55459a2ecff0828d94ec476
marijntepas <marijntepas@gmail.com>
2022-08-04 18:41:57 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -3,7 +3,8 @@ echo on
 Echo zipping...
 
 del "D:\Program Files (x86)\Steam\steamapps\common\InvisibleInc\mods\MOD_DOC\scripts.zip"
-cd ./scripts/
+e:
+cd E:\Users\Marijn\workspace-lua\MOD_DOC\scripts
 "E:\Program Files (x86)\WinRAR\winrar.exe" a -tzip "D:\Program Files (x86)\Steam\steamapps\common\InvisibleInc\mods\MOD_DOC\scripts.zip" "."
 
 echo Done!
