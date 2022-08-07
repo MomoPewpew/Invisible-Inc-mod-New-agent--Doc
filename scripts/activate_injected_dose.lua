9736ff2902c74a87e0e614584e3f62fb2bcf815d
marijntepas <marijntepas@gmail.com>
2022-08-07 21:45:39 +0200

    Added dynamic icons to activate dose

--- scripts/activate_injected_dose.lua
+++ scripts/activate_injected_dose.lua
@@ -21,10 +21,26 @@ local activate_injected_dose =
 		getName = function( self, sim, unit, userUnit )
 			return self.name
 		end,
+
+        unitDataHasAbility = function( unitData, abilityID )
+            if unitData.abilities then
+                for i,ability in ipairs(unitData.abilities) do
+                    if ability:getID() == abilityID then
+                        return true
+                    end
+                end
+            end
+        
+            return false
+        end,
 	
 		createToolTip = function( self, sim, abilityOwner, abilityUser, targetUnitID )
             local targetUnit = sim:getUnit( targetUnitID )
             local unitData = targetUnit:getTraits().drugpistoldose
+            if self:unitDataHasAbility(unitData, "use_medgel") then self.profile_icon = "gui/icons/item_icons/items_icon_small/icon-item_stim_small.png"
+            elseif self:unitDataHasAbility(unitData, "use_aggression") then self.profile_icon = "gui/icons/item_icons/items_icon_small/icon-item_defibulator_small.png"
+            else self.profile_icon = "gui/icons/item_icons/items_icon_small/icon-item_stim_small.png"
+            end
 			return (STRINGS.MOD_DOC.ABILITIES.ABILITY_ACTIVATE_INJECTED_DOSE_ACTIVATE .. unitData.name)
 		end,
 	
@@ -54,7 +70,11 @@ local activate_injected_dose =
 		end,
 		
 		executeAbility = function( self, sim, unit, userUnit, targetUnitID )
-
+            local targetUnit = sim:getUnit( targetUnitID )
+            local unitData = targetUnit:getTraits().drugpistoldose
+            --newUnit = simfactory.createUnit( unitData, sim )
+            --sim:spawnUnit( newUnit )
+            --self:doInjection( sim, newUnit, userUnit, targetUnitID )
         end
 	}
 return activate_injected_dose
